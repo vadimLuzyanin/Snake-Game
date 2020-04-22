3f298a28a4ee3e22d10f792bb612d23c3a9d3f04
vadimLuzyanin <luzianin999@inbox.ru>
2020-04-22 08:14:56 +0300

    Updates

---
+++ precache-manifest.3fc29054549496702b351b3cbdc79eca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79d10d8fb1ab2f066a63897ff2df5aa7",
+    "url": "/Snake-Game/index.html"
+  },
+  {
+    "revision": "269133b7f5d3ca836146",
+    "url": "/Snake-Game/static/js/2.90f7cc6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Snake-Game/static/js/2.90f7cc6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2a2fb47599b85452784",
+    "url": "/Snake-Game/static/js/main.7d60e009.chunk.js"
+  },
+  {
+    "revision": "a4ff33b962509a424f30",
+    "url": "/Snake-Game/static/js/runtime-main.887a5424.js"
+  }
+]);
